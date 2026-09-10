d4b08f4a9ae9aa9d0ecc3f054b7559bbc774694d
Panda Warmling <lhwarmling@hotmail.com>
2026-09-09 21:41:48 -0300

    aula09_09

---
+++ script.js
@@ -0,0 +1,30 @@
+//atribuiçao
+let meunome="Luiz";
+
+//redeclaração
+meunome="luiz henrique"
+
+let idade = 28;
+const deMaior = false;
+const mensagem = `Meu nome é ${meunome} e tenho ${idade} anos`;
+
+//atribuiçao objeto
+const usuario={
+    nome:"jose",
+    idade:35
+};
+
+console.log(usuario.idade)
+
+//redeclaraçao objeto
+usuario.idade=36;
+
+
+
+console.log(usuario.idade);
+console.log(usuario);
+console.log(mensagem);
+
+const frutas=["maça", "banana","morango"];
+console.log(frutas);
+console.log(frutas[1]);
